6645873ca0d69273d8e9c85add4c4ef63a770563
Darshan Balakrishnan <143161270+Krishnan200101@users.noreply.github.com>
2025-01-09 11:56:46 +0530

    Create app.php

---
+++ config/app.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application, which will be used when the
+    | framework needs to place the application's name in a notification or
+    | other UI elements where an application name needs to be displayed.
+    |
+    */
